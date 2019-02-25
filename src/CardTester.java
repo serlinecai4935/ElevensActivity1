cf56b99e4e3ec3d7371a716eb17aaac359f8e5ee
serlinecai4935 <scai4935@bths.edu>
2019-02-24 23:05:07 -0500

    one

--- src/CardTester.java
+++ src/CardTester.java
@@ -2,4 +2,14 @@
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    public static void main(String[] args) {
+        Card one = new Card("3", "Hearts", 3);
+        Card two = new Card("2", "Spades",3);
+        Card three = new Card("3", "Diamond", 3);
+
+        System.out.println("Does one match two? " + one.matches(two));
+        System.out.println("Does one match three? " + one.matches(three));
+        System.out.println("Suit of Card Two: " + one.suit());
+        System.out.println("Card two is " + one.toString());
+    }
 }
